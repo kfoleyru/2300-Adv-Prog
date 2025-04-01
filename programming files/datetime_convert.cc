e3ddf1060e9fd6db84f88064d9a9f4a272edac24
LincolnR1 <lincoln.rutkowski@uvm.edu>
2025-04-01 11:05:13 -0400

    fixed bug in datetime_convert.cc

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -10,7 +10,7 @@ int main()
     std::istringstream ss(date_str);
     ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
-    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %h:%m:%s");
+    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str();
 
     std::cout << formatted_date << std::endl;
