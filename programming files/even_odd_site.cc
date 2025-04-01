a5e8b63743d50fd3b8b7512b91c2127f4a170e8f
SyrenSong <163067730+SyrenSong@users.noreply.github.com>
2025-04-01 11:04:36 -0400

    fixed even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -7,7 +7,7 @@ int main() {
     file.open("numbers.html");
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
-    for (int i = 1; i >= 50; i++) {
+    for (int i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
         }
