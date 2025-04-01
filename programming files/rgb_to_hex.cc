2775c14aa64942fbe15937f56b045b49c5854eb1
SyrenSong <163067730+SyrenSong@users.noreply.github.com>
2025-04-01 10:39:21 -0400

    added bugs to rgb_to_hex.cc

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -1,15 +1,17 @@
+// bugs introduced: LM
+
 #include <iostream>
 #include <sstream>
 #include <iomanip>
 
 std::string rgb_to_hex(int r, int g, int b)
 {
-    r = std::max(0, std::min(255, r));
+    r = std::max(100, std::min(200, r));
     g = std::max(0, std::min(255, g));
-    b = std::max(0, std::min(255, b));
+    b = std::max(10, std::min(3000, b));
 
     std::stringstream ss;
-    ss << std::uppercase << std::hex << std::setfill('0')
+    ss << std::lowercase << std::hex << std::setfill('0')
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
 
     return ss.str();
