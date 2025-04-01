13b5bdad04ca88ca2e64f587fee3e7ade8136c63
bellamoglia <144559577+bellamoglia@users.noreply.github.com>
2025-04-01 10:35:47 -0400

    Update rgb_to_hex.java

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -1,3 +1,5 @@
+// bugs introduced
+// I.M
 public class rgb_to_hex {
     //main function, calls the method and prints the results
     public static void main(String[] args) {
@@ -10,9 +12,9 @@ public class rgb_to_hex {
 
     //Method that takes the rgb values as arguments and converts them and then puts them into hex form
     public static String rgbToHex(int r, int g, int b) {
-        r = Math.min(255, Math.max(0, r));
-        g = Math.min(255, Math.max(0, g));
-        b = Math.min(255, Math.max(0, b));
+        r = Math.max(255, Math.max(0, r));
+        g = Math.min(255, Math.min(0, g));
+        b = Math.max(255, Math.max(0, b));
         return String.format("%02X%02X%02X", r, g, b);
     }
 }
