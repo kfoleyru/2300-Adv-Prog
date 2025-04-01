a4c5cf514f267cf95931d8fe83e9de02a01ad515
LincolnR1 <lincoln.rutkowski@uvm.edu>
2025-04-01 10:36:22 -0400

    added bug sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,11 +1,11 @@
-
+//bug introduced: LR
 // This method sums the even terms of the Fibonacci sequence from 0 to 4,000,000
 public class sum_even_fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
         while (b < 4000000) {
-            if (b % 2 == 0) {
+            if (b % 2 != 0) {
                 sumEven += b;
             }
             int temp = b;
