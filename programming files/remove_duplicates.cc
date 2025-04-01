561671064a8904f0df6eebdb1723c718309deb40
SyrenSong <163067730+SyrenSong@users.noreply.github.com>
2025-04-01 11:00:51 -0400

    fixed remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -11,7 +11,7 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     sort(arr.begin(), arr.end());
     int n = arr.size();
     for(int i=0; i<n; i++){
-        if(i == 0 || arr[i] == arr[i-1]){
+        if(i == 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
         else{
