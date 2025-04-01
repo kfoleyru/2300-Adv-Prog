0bdfb6805a1b75ff3e679ac6b6e0abba97828fa9
LincolnR1 <lincoln.rutkowski@uvm.edu>
2025-04-01 10:35:01 -0400

    added bug sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -5,7 +5,7 @@ int main()
     int a = 0, b = 1;
     int sum_even = 0;
     while (b < 4000000) {
-        if (b % 2 == 0) {
+        if (b / 2 == 0) {
             sum_even += b;
         }
         int temp = b;
