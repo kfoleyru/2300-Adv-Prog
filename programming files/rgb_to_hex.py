0f80d762c06d0cbc12474ec57b6d95340f6efc18
bellamoglia <144559577+bellamoglia@users.noreply.github.com>
2025-04-01 10:36:50 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,11 @@
+# bugs introduced
+# I.M
+
 # function to translate rgb to hex colors and returns in the proper format
 def rgb_to_hex(r, g, b):
-    r = max(0, min(255, r))
-    g = max(0, min(255, g))
-    b = max(0, min(255, b))
+    r = min(0, min(255, r))
+    g = min(0, min(255, g))
+    b = min(0, min(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
