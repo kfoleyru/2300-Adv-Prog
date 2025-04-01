b01b26f43b45ea6de80a632a22e92ecf22b9325f
sadieellwood <sellwood@uvm.edu>
2025-04-01 11:02:51 -0400

    Update rgb_to_hex.java

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -12,9 +12,9 @@ public class rgb_to_hex {
 
     //Method that takes the rgb values as arguments and converts them and then puts them into hex form
     public static String rgbToHex(int r, int g, int b) {
-        r = Math.max(255, Math.max(0, r));
-        g = Math.min(255, Math.min(0, g));
-        b = Math.max(255, Math.max(0, b));
+        r = Math.max(255, Math.min(0, r));
+        g = Math.max(255, Math.min(0, g));
+        b = Math.max(255, Math.min(0, b));
         return String.format("%02X%02X%02X", r, g, b);
     }
 }
